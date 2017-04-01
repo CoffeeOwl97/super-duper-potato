4cdf1235db7a2f688fe63882ef67c4d3f9085ed5
Connor Bradley <connorbradley@Connors-MacBook-Pro.local>
2017-04-01 20:00:38 +0100

    Now compresses the image and redirects you

---
+++ public/javascripts/index.js
@@ -0,0 +1,9 @@
+// room_id = 4001 // You can set the id here for each store dynamically
+// // Connect to socket
+// var socket = io.connect('https://whateveryourappnameis.herokuapp.com');
+// // Emmit message joining room
+// socket.emit('join room', {
+//         message: "Joining Room",
+//         username: "Connor",
+//         room: room_id
+// });
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var multer = require('multer');
 var router = express.Router();
+var http = require('http');
+//var server = http.createServer(express);
+//
+//settings for uploading
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, './uploads')
@@ -10,31 +14,37 @@ var storage = multer.diskStorage({
   }
 })
 
+//server.listen(3000);
+//io.sockets.on('connection')
+
+
+//Settings for uploading
 var upload = multer({ storage: storage })
 
 const {execFile} = require('child_process');
 const guetzli = require('guetzli');
 
-execFile(guetzli, ['input.jpg', 'output.jpg'], err => {
+//Compress the image
+function minify(file, res){
+  execFile(guetzli, [file.path, './uploads/output.jpg'], err => {
     console.log('Image minified!');
-});
+    res.render('index', {title: 'Express', currentStatus: 'Heres your sexy shrunken file'});
+})};
 
 
-var uploading = multer({
-  dest: __dirname + '../public/uploads/',
-  limits: {fileSize: 10000000, files:1},
-});
-
+//Submit image
 router.post('/upload', upload.single("image"), function(req, res)
 {
   var file = req.file;
   console.log(file);
-  res.end();
+  minify(file, res);
+  res.render('index', {title: 'Express', currentStatus: 'File Uploaded'});
 });
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', currentStatus: 'Upload in progress'});
 });
 
 module.exports = router;
